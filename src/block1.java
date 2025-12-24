d4167bba7d7cae8fef3285cbcaf6670121bc682b
ku28 <ku28sh07@gmail.com>
2025-12-24 10:43:05 +0530

    second commit

---
+++ src/block1.java
@@ -0,0 +1,11 @@
+public class block1 {
+    int a = 10;
+    static int b = 20;
+    public static void main(String[] args) {
+        int c=30;
+        block1 A = new block1();
+        System.out.println(A.a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
